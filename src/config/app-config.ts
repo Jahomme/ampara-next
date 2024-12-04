83642f00479575925d28c42a948e766f6f920664
Jadilson <jadilsonigor@gmail.com>
2024-12-03 23:57:22 -0300

    :wrench: config api_url

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -1,5 +1,5 @@
-// export const API_URL = 'https://ampara-strapi-base.onrender.com';
-export const API_URL = 'http://localhost:1337';
+export const API_URL = 'https://ampara-strapi-base.onrender.com';
+// export const API_URL = 'http://localhost:1337';
 export const CONTENTS_URL = `${API_URL}/api/faq-contents?populate=*`;
 export const DIARIES_URL = `${API_URL}/api/diaries?populate=*`;
 export const DIARY_URL = `${API_URL}/api/diaries`;
